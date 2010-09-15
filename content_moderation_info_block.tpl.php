91f38eadd472f7bcc272e988e876d3b69cc871a3
Arthur <foelsche@palantir.net>
2010-09-15 17:32:51 -0400

    Major commit that moves this module toward d7  * sql changes: in content_moderation_revision_state, "state" was renamed "state_name" to keep things consistent  * most of the actual sql commands have been converted to D7  * I unfortunately got side tracked trying to help clean up some of the architecture of the module- one    major change is that I'm loading all the revision information during node_load. Because D7 does    entity caching, I think it makes more sense to attach this data to the node which reduces the number    of function calls that happen to find various data out about a specific node (eg: current state, live, etc)  * I implemented content_moderation_live_state() and content_moderation_none_state() where I found it needed it  * I've started doing function reorganizing putting drupal hooks at the top of the module  * I've started renaming some of the _content_moderation functions that I could either simplify or reimplement    as utility functions. This is not a big win outside of maybe helping with readability
    TODO:
     * I need to cleanup some of the revision handling - I missunderstood how the node revision is related
       to the current state of the node
     * I need to cleanup some of the functions that I can factor out now that a node carries all of its
       moderation history
     * Access control for state changes is still something that I need to work on. The node state change
       form builds its options without doing permission checking.
     * Problably should break theming functions out into a seperate file

--- content_moderation_info_block.tpl.php
+++ content_moderation_info_block.tpl.php
@@ -1,66 +1,51 @@
 <?php
 // $Id$
 
-# Copyright (c) 2010 Impressive.media
-# Author: Eugen Mayer
-/*
- * Usable values
- * $node : currently viewed node
- * $state: workflow state of the currently viewed node revision
- * $live: live version of the node
- * $live_link: revision and link to the live node
- * $links: actions on the different revisions (compare_with_live,compare,delete_revision, edit_revision, edit_live)
+// Copyright (c) 2010 Impressive.media
+// Author: Eugen Mayer
+
+
+/**
+ * @file
+ * Provides formating for the history block
+ *
+ * This nees some real cleanup to get the logic out
+ * of the template. Much of this has been moved up to the
+ * calling functions, but there is still more to be done.
  */
 
-
-$current_user = user_load($node->revision_uid);
-
-$edit_icon = '<span class="tinyicon t_editicon"></span>';
-$rev_icon = '<span class="tinyicon t_revisionicon"></span>';
-$edit_state_icon = '<span class="tinyicon t_changestateicon"></span>';
-$delete_icon = '<span class="tinyicon t_deleteicon"></span>';
-$view_icon = '<span class="tinyicon t_viewicon"></span>';
-
-
-/* actions */
-if(_content_moderation_statechange_allowed($node->vid)) {
-  $edit_state = l($edit_state_icon,$links['edit_state'],array('html' => true, 'attributes' => array( 'title' => t('Edit the state of this revision.') ) ));
-}
-
-if($live != NULL) {
-  $view_live_link = l($view_icon,$links['live_view'],array('html' => true, 'attributes' => array( 'title' => t('View live revision') ) ));
-
-}
-
-$view_current_link = l($view_icon,"node/{$node->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
-$current_rev_link = l($node->vid,"node/{$node->nid}/revisions/{$node->vid}/view",array('html' => true, 'attributes' => array( 'title' => t('View this revision.') ) ));
-
-if(module_exists('diff')){
-  $compare_live = l($rev_icon,$links['compare_with_live'],array('html' => true, 'attributes' => array( 'title' => t('Compare this revision with the live revision.') ) ));
-  $compare = l($rev_icon,$links['compare'],array('html' => true, 'attributes' => array( 'title' => t('List all revisions.') ) ));
-}
-// TODO: first see how revision deleting, esp. the live ones, should be handled
-//$delete_current = l($delete_icon,$links['delete_revision'],array('html' => true, 'attributes' => array( 'title' => t('Delete this revision') ) ));
+extract($arguments);
 
 ?>
 <div id="content_moderation">
-
   <h4><?php print t('Live')?></h4>
-  <?php if($live != NULL) {
+  <?php if ($live != NULL) {
     $live_user = user_load($live->revision_uid);
     ?>
-    <div class="info live_info"><?php print $live->vid?>: <?=$live_link.$view_live_link.$compare?><br/>
-    <span class="details">&raquo; <?php print format_date($live->revision_timestamp, 'small')?> (<?php print $live_user->name?>)</span>
+    <div class="info live_info"><?php print $live->vid .': ' . $live_link . $view_live_link . $compare ;?>
+
+    <br/>
+    <span class="details">&raquo; <?php print format_date($live->revision_timestamp, 'small')?> (<?php print $live_user->name ?>)</span>
     </div>
   <?php }  else { ?>
     <div class="info live_info"><?php print t('nothing approved yet');?></div>
   <?php } ?>
 
 
-  <?php if($live == NULL || $node->vid != $live->vid) {?>
+  <?php if ($live == NULL || $node->vid != $live->vid) { ?>
   <h4><?php print t('Viewing')?></h4>
-  <div class="info"><label><?php print $current_rev_link?>:</label> (<?php print $state?>) <?php print $view_current_link.$edit_state.$compare_live.$delete_current?><br/>
-    <span class="details">&raquo; <?php print format_date($node->revision_timestamp, 'small')?> (<?php print $current_user->name?>)</span>
+  <div class="info">
+    <label>
+      <?php print t('Revision: !revision', array('!revision' => $current_rev_link)); ?>:
+    </label>
+    <?php
+      print t('(State: !state)', array('!state' => $state));
+      print $edit_state;
+      isset($compare_with_live) ? print $compare_with_live : NULL;
+      print $delete_current;
+    ?>
+    <br/>
+    <span class="details">&raquo; <?php print format_date($node->revision_timestamp, 'small'); ?> (<?php print $node->name; ?>)</span>
     <span class="details state">&raquo; Status: <?php print t($state)?></span>
   </div>
   <?php }?>
